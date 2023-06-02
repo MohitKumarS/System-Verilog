ede554ed7307471d4da388b8a1a9c73aeedc4cab
Mohit Kumar Sahoo <109848538+MohitKumarS@users.noreply.github.com>
2023-06-02 22:00:05 +0530

    Create Task.sv

---
+++ Week_1/Day_4/Task.sv
@@ -0,0 +1,13 @@
+// Code your testbench here
+// or browse Examples
+module task_sum(input [7:0]a,b, output reg [7:0]s);
+  
+  always @(a,b)
+    sum_2_var(a,b,s);
+  
+  task sum_2_var(input[7:0]a,b, output reg [7:0]s);
+    begin
+      s = a+b;
+    end
+  endtask
+endmodule
