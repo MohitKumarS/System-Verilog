d131ec20c8e298dcd557199dbb1341382937f6cb
Mohit Kumar Sahoo <109848538+MohitKumarS@users.noreply.github.com>
2023-06-02 22:07:16 +0530

    Update Task_design.sv

--- Week_1/Day_4/Task_design.sv
+++ Week_1/Day_4/Task_design.sv
@@ -19,27 +19,3 @@ module task_sum(input [7:0] a,b, output reg [7:0] s);
 endmodule
 
 
-module task_count(input [7:0] data_in, output reg [3:0] data_out);
-  
-  integer i;
-  
-  always @(data_in,data_out)
-    
-    count_is_in_byte(data_in,data_out);
-  
-  task count_is_in_byte(input [7:0] data_in, output reg [3:0] data_out);
-    
-    begin 
-      data_out = 0;
-      
-      for(i=0;i<8;i++)
-        
-        if(data_in[i] == 1)
-          
-          data_out = data_out+1;
-    end
-    
-  endtask
-  
-  
-endmodule
