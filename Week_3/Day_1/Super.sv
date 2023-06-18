679fc53a9ed985f26cd85a3d49d61c02598e44f5
Mohit Kumar Sahoo <109848538+MohitKumarS@users.noreply.github.com>
2023-06-18 20:45:27 +0530

    Create Super.sv

---
+++ Week_3/Day_1/Super.sv
@@ -0,0 +1,30 @@
+class parent;
+  int a;
+  
+  function display();
+    $display("In parent : a = %0d",a);
+  endfunction
+endclass
+
+class child extends parent;
+  int b;
+  
+  function display();
+    $display("In child  : b = %0d",b);
+    super.display();
+  endfunction
+endclass
+  
+module inheritence;
+  initial begin
+    child c = new();
+    c.a = 10;
+    c.b = 20;
+    c.display();
+  end
+endmodule
+
+
+// output
+// In child  : b = 20
+// In parent : a = 10
