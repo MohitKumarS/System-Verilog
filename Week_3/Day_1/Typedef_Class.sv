566b9baf05606bb7e800ee9061173cc451de3e68
Mohit Kumar Sahoo <109848538+MohitKumarS@users.noreply.github.com>
2023-06-18 20:45:02 +0530

    Create Typedef_Class.sv

---
+++ Week_3/Day_1/Typedef_Class.sv
@@ -0,0 +1,22 @@
+typedef class c2;
+//class-1
+class c1;
+  c2 c;    //using class c2 handle before declaring it.
+endclass
+
+//class-2
+class c2;
+  c1 c;
+endclass
+ 
+module typedef_class;
+  initial begin
+    c1 class1;
+    c2 class2;
+    $display("Inside typedef_class");
+  end
+endmodule
+  
+
+// output
+// Inside typedef_class
