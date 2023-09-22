f48c26b4b7d81ff526d06811f424f1570e8c1a56
Mohit Kumar Sahoo <109848538+MohitKumarS@users.noreply.github.com>
2023-09-22 10:16:27 +0530

    Create testbench.sv

---
+++ Week_4/PROJECT/testbench.sv
@@ -0,0 +1,19 @@
+
+`include "adder_intf.sv"
+
+`include "common.sv"
+
+`include "adder_tx.sv"
+
+`include "adder_gen.sv"
+
+`include "adder_bfm.sv"
+
+`include "adder_agent.sv"
+
+`include "adder_env.sv"
+
+`include "adder_test.sv"
+
+`include "top.sv"
+
